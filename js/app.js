f91e6317b32fd7ad8292568a4295cc27f37ec76c
fmiguezo <fmiguezol@proton.me>
2024-11-06 11:58:49 -0300

    Update collapsible feature text for better user experience

--- js/app.js
+++ js/app.js
@@ -5,8 +5,10 @@ collapse.forEach((item) => {
     const content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
+      this.textContent = "Ver más";
     } else {
       content.style.display = "block";
+      this.textContent = "Ver menos";
     }
   });
 });
